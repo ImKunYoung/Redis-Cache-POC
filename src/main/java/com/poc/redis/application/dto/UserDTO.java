f93f573acccc7400a4d41fb16aa65b893c737687
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-24 19:36:05 +0900

    fix(#11): UserDTO 생성자 관련 에러 해결

--- src/main/java/com/poc/redis/application/dto/UserDTO.java
+++ src/main/java/com/poc/redis/application/dto/UserDTO.java
@@ -2,12 +2,14 @@ package com.poc.redis.application.dto;
 
 import com.poc.redis.domain.model.User;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 /**
  * A DTO representing a user, with only the public attributes.
  */
+@NoArgsConstructor
 @Data
 public class UserDTO implements Serializable {
 
@@ -17,11 +19,8 @@ public class UserDTO implements Serializable {
 
     private String login;
 
-
     public UserDTO(User user) {
         this.id = user.getId();
-        // Customize it here if you need, or not, firstName/lastName/etc
         this.login = user.getLogin();
     }
-
 }
